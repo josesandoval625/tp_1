33ac7820b58f0c08d95a366dba20ec44e09b4489
Luis Sandoval <luisandovalhorna@gmail.com>
2022-09-20 01:37:34 -0300

    en desarrolo la clase Cjefe

--- tp_uno/Cjefe.cpp
+++ tp_uno/Cjefe.cpp
@@ -1,1 +1,78 @@
 #include "Cjefe.h"
+
+Cjefe::Cjefe(string nombre,string apellido,string dni,string telefono)
+	:Nombre(nombre),Apellido(apellido),DNI(dni),Telefono(telefono)
+{
+	Disponibilidad = false;
+	NumeroMaximoDeProyectos = 0;
+	NumeroDeProgramadoresAsuCargo=3;
+	NumeroActualDeProgramadores=0;
+	NumeroActualDeProyectos=0;
+	ListaDeProyectosAsuCargo = new Cproyecto * [NumeroMaximoDeProyectos];
+	ListaDeprogramadoresQueJefeTieneAcargo = new Cprogramador * [NumeroDeProgramadoresAsuCargo];
+	for (int i = 0; i < NumeroMaximoDeProyectos; i++)
+	{
+		ListaDeProyectosAsuCargo[i] = nullptr;
+	}
+	for (int i = 0; i < NumeroDeProgramadoresAsuCargo; i++)
+	{
+		ListaDeprogramadoresQueJefeTieneAcargo[i] = nullptr;
+	}
+}
+
+Cjefe::~Cjefe()
+{
+}
+
+string Cjefe::getNombre()
+{
+	return this->Nombre;
+}
+
+string Cjefe::getApellido()
+{
+	return this->Apellido;
+}
+
+bool Cjefe::getDisponibilidad()
+{
+	return this->Disponibilidad;
+}
+
+bool Cjefe::VerificarDisponibilidad()
+{
+	if (NumeroActualDeProyectos < NumeroMaximoDeProyectos)
+	{
+		Disponibilidad = true;
+	}
+	else
+	{
+		Disponibilidad = false;
+	}
+	return Disponibilidad;
+}
+
+void Cjefe::AsignarNumeroMaximoDeProyectos(int numeroMaximoDeProyectos)
+{
+	this->NumeroMaximoDeProyectos = numeroMaximoDeProyectos;
+}
+
+void Cjefe::AsignarNumeroMaximoDeProgramadores(int numeroMaximoDeProgramadores)
+{
+	this->NumeroDeProgramadoresAsuCargo = numeroMaximoDeProgramadores;
+}
+
+void Cjefe::asignarProyecto(Cproyecto* proyecto)// disponibilidad del jefe si bool disponible=1 de lo contrario =0=false
+{
+	if (VerificarDisponibilidad() == true)
+	{
+		ListaDeProyectosAsuCargo[NumeroActualDeProyectos] = proyecto;
+		NumeroActualDeProyectos++;
+	}
+	else
+	{
+		cout << "el jefe: " << getNombre() << " " << getApellido() << " no esta disponible. " << endl;
+	}
+	
+
+}
--- tp_uno/Cjefe.h
+++ tp_uno/Cjefe.h
@@ -1,5 +1,32 @@
 #pragma once
+#include<string>
+#include"Cproyecto.h"
+#include"Cprogramador.h"
+using namespace std;
 class Cjefe
 {
+private:
+	string Nombre;
+	string Apellido;
+	string DNI;
+	string Telefono;
+	bool Disponibilidad;// false= si ya esta asignado a un proyecto
+	Cproyecto** ListaDeProyectosAsuCargo;
+	int NumeroMaximoDeProyectos;
+	Cprogramador** ListaDeprogramadoresQueJefeTieneAcargo;
+	int NumeroDeProgramadoresAsuCargo;
+	int NumeroActualDeProgramadores;
+	int NumeroActualDeProyectos;
+public:
+	Cjefe(string,string,string,string);
+	~Cjefe();
+	string getNombre();
+	string getApellido();
+	bool getDisponibilidad();
+	bool VerificarDisponibilidad();
+	void AsignarNumeroMaximoDeProyectos(int numeroMaximoDeProyectos);
+	void AsignarNumeroMaximoDeProgramadores(int numeroMaximoDeProgramadores);
+	void asignarProyecto(Cproyecto* proyecto);
+
 };
 
